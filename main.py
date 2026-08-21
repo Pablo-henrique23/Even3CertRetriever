85a8aa005158661959fdc55d195956f816779f56
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2026-08-21 01:14:25 -0300

    mudança boba

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import *
 
-print("[#] Antes de iniciar, troque o cookie em utils.py\n\n")
+print("[#] Trocou o cookie, ne?\n\n")
 
 first_cert_page = get_certificate_list()
 cert_select_for_payment_page = get_select_for_payment_page(first_cert_page)
